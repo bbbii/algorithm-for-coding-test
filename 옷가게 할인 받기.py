20deab341300cdb234d1a3f271959b75a5c46412
bbbii <gudwls1326@naver.com>
2022-11-02 00:45:38 +0900

    옷가게 할인 받기

---
+++ 옷가게 할인 받기.py
@@ -0,0 +1,10 @@
+def solution(price):
+    if price >= 500000:
+        answer = price*0.80
+    elif price >= 300000 and price < 500000:
+        answer = price*0.90
+    elif price >= 100000 and price < 300000:
+        answer = price*0.95
+    else:
+        answer = price
+    return int(answer)
