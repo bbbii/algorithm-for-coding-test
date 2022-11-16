a3419095aa423c7ef356d675ed00527a3c08a708
bbbii <gudwls4829123@gmail.com>
2022-11-16 18:06:22 +0900

    등수 매기기

---
+++ program/level0/등수 매기기.py
@@ -0,0 +1,9 @@
+def solution(score):
+    dict = {}
+    q = []
+    for i in score:
+        q.append(sum(i) / 2)
+    for index, avg in enumerate(sorted(q, reverse = True), start = 1):
+        if avg not in dict:
+            dict[avg] = index
+    return [dict[avg] for avg in q]
