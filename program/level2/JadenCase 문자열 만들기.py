9637d8a7b7be83f05b6517a8b7230ebfaeb02523
bbbii <gudwls4829123@gmail.com>
2022-12-15 23:48:58 +0900

    JadenCase 문자열 만들기

---
+++ program/level2/JadenCase 문자열 만들기.py
@@ -0,0 +1,10 @@
+def solution(s):
+    a = list(s)
+    b = s.lower()
+    c = list(b)
+    c[0] = a[0].upper()
+    for i in range(len(s)-1):
+        if a[i] == ' ':
+            c[i + 1] = a[i + 1].upper()
+    answer = ''.join(c)
+    return answer
