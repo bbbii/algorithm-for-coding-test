532a0155ed08229ccc3b6b1a4e11f1ad2d6e4a5a
bbbii <gudwls4829123@gmail.com>
2022-12-30 16:05:53 +0900

    타겟 넘버

---
+++ program/level2/타겟 넘버.py
@@ -0,0 +1,13 @@
+from collections import deque
+def solution(numbers, target):
+    answer = 0
+    queue = deque([(0, 0)])
+    while queue:
+        s, l = queue.popleft()
+        if l > len(numbers):
+            break
+        elif l == len(numbers) and s == target:
+            answer += 1
+        queue.append((s + numbers[l - 1], l + 1))
+        queue.append((s - numbers[l - 1], l + 1))
+    return answer
