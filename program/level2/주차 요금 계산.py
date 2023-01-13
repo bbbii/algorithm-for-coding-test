da028c52844bac7a7273c0d78a71fccaca4e27c0
bbbii <gudwls4829123@gmail.com>
2023-01-13 21:21:53 +0900

    주차 요금 계산

---
+++ program/level2/주차 요금 계산.py
@@ -0,0 +1,61 @@
+from collections import deque
+from collections import OrderedDict
+from datetime import datetime
+import time
+import math
+def time_minus(time):
+    all_time = 0
+    all_time += int(time[:2])*60
+    all_time += int(time[3:5])
+    return all_time
+def solution(fees, records):
+    temp_answer = []
+    answer = []
+    temp_list = []
+    dict = {}
+    IN_dict = {}
+    time_dict = {}
+    dict = OrderedDict()
+    IN_dict = OrderedDict()
+    time_dict = OrderedDict()
+    for i in range(len(records)):
+        temp = records[i].split(' ')
+        temp_list.append(temp)
+    for i in range(len(temp_list)):
+        total_time = 0
+        if (temp_list[i][1] not in dict) and temp_list[i][2] == 'OUT':
+            total_time = time_minus(temp_list[i][0]) - IN_dict[temp_list[i][1]]
+            dict[temp_list[i][1]] = total_time
+            dict.move_to_end(temp_list[i][1])
+            del IN_dict[temp_list[i][1]]
+        elif (temp_list[i][1] in dict) and temp_list[i][2] == 'OUT':
+            total_time = time_minus(temp_list[i][0]) - IN_dict[temp_list[i][1]]
+            dict[temp_list[i][1]] += total_time
+            dict.move_to_end(temp_list[i][1])
+            del IN_dict[temp_list[i][1]]
+        elif temp_list[i][2] == 'IN':
+            IN_dict[temp_list[i][1]] = time_minus(temp_list[i][0])
+        else:
+            dict[temp_list[i][1]] = time_minus(temp_list[i][0])
+    for i in IN_dict:
+        IN_dict[i] = 1439 - IN_dict[i]
+        if i in dict:
+
+            dict[i] += IN_dict[i]
+        else:
+            dict[i] = IN_dict[i]
+    dict2 = OrderedDict(sorted(dict.items()))
+    for i in dict2:
+        temp_answer.append(dict[i])  
+    for i in temp_answer:
+        i = int(i)
+        if i <= fees[0]:
+            answer.append(fees[1])
+        else:
+            if i - fees[0] % fees[2] != 0:
+                l = int(fees[1] + (math.ceil((i - fees[0]) / fees[2]) * fees[3]))
+                answer.append(l)
+            else:
+                l = int(fees[1] + (((i-fees[0]) / fees[2]) * fees[3]))
+                answer.append(l)
+    return answer
