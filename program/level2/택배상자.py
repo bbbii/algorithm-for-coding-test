a8b8f53010a29b2e912d50bce8039079efdc7ebe
bbbii <gudwls4829123@gmail.com>
2023-01-29 19:11:00 +0900

    택배상자

---
+++ program/level2/택배상자.py
@@ -0,0 +1,12 @@
+def solution(order):
+    temp = []
+    i, now = 1, 0
+    while i != len(order) + 1:
+        temp.append(i)
+        while temp[-1] == order[now]:
+            now += 1
+            temp.pop()
+            if len(temp) == 0:
+                break
+        i += 1
+    return now
